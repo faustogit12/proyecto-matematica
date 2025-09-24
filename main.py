a95ce3c71e70f16c908a2aff9188cf07aa4c0b31
GuidoHermann <guidohermanndev@gmail.com>
2025-09-24 17:37:53 -0300

    contador binario

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import random
 
+def contador_binario():
+    # Pedimos el límite al usuario
+
+    limite = int(input("Ingresa hasta qué número quieres contar en binario: "))
+    print("=== Contador Binario ===")
+
+    # Repetitiva: usamos un ciclo while
+    i = 0
+
+    while i <= limite:
+        # Secuencial: mostramos decimal y binario usando condicionales si queremos formato
+        binario = ""
+        n = i
+        if n == 0:
+         binario = "0"
+        else:
+            while n > 0:  # repetitiva
+                binario = str(n % 2) + binario
+                n = n // 2
+        print(f"{i} en binario es {binario}")
+        i += 1
 
 def binario_a_decimal(): ## funcion de binario a decimal
     binario = input("Ingrese un numero binario (Solo 0 y 1): \n")
